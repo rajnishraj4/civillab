962f80682e2bd96e33fd4a52be8b2bc6e70a0781
Rajnish Raj <rajnishraj4@gmail.com>
2025-08-30 01:41:25 +0530

    Initialised project

---
+++ src/test/java/com/tech/adira/app/management/civillab/CivillabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.adira.app.management.civillab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivillabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
